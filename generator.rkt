7bd1a3ecb4b2db2c5e07712246be1d3da897afac
Drew Bronson <bronsonboy96@gmail.com>
2017-06-08 15:51:31 -0500

    Structs aren't first-degree objects. RIP.

--- generator.rkt
+++ generator.rkt
@@ -4,42 +4,44 @@
 (require "tile-database.rkt")
 (require "choice.rkt" "assert.rkt")
 
-(define (rotate tile-1)
-  (tile (gimp-item-transform-rotate-simple (tile-image tile-1) 0 #t 600 600)
-        (tile-east tile-1) (tile-south tile-1)
-        (tile-west tile-1) (tile-north tile-1)))
+(define (rotate tile-1 iterations)
+  (if (equal? 0 iterations) tile
+      (rotate (tile (gimp-item-transform-rotate-simple (tile-image tile-1)
+                                                       0 #t 600 600)
+                    (tile-east tile-1) (tile-south tile-1)
+                    (tile-west tile-1) (tile-north tile-1))
+              (- iterations 1))))
 
-(define (valid! tile-1))
+(define (add-rotations tiles-db)
+  (let ([new-db '()])
+    (for ([i 4]
+          [each-tile tiles-db])
+      (cons new-db (rotate each-tile i)))
+    new-db))
 
-;; (define (valid tile-1 north-neighbor east-neighbor num-rotations)    ;;
-;;   (assert (or (and (or (null? north-neighbor)                        ;;
-;;                        (equal? (tile-south north-neighbor)           ;;
-;;                                (tile-north tile-1)))                 ;;
-;;                    (or (null? east-neighbor)                         ;;
-;;                        (equal? (tile-west east-neighbor)             ;;
-;;                                (tile-east tile-1))))                 ;;
-;;               (and (< 3 num-rotations)                               ;;
-;;                    (valid (rotate tile) north-neighbor east-neighbor ;;
-;;                           (+ 1 num-rotations))))))                   ;;
+(define (valid! a-tile north-neighbor east-neighbor)
+  (equal! (tile-north a-tile) (tile-south north-neighbor))
+  (equal! (tile-east a-tile) (tile-west east-neighbor)))
 
-
-;; (define (valid-map map-tiles)
-;;   (let ([width (length map-tiles)]
-;;         [height (length (list-ref map-tiles 0))])
-;;     (for/and ([column (range width)]
-;;               [row (range height)])
-;;       (valid (list-ref (list-ref map-tiles column) row)
-;;              (if (equal? row (- height 1)) null
-;;                  (list-ref (list-ref map-tiles (+ column 1)) (+ row 1)))
-;;              (if (equal? column (- width 1)) null
-;;                  (list-ref (list-ref map-tiles (+ column 1)) (+ row 1)))
-;;              0))))
+(define (map-valid! columns)
+  (let ([width (length columns)]
+        [height (length (list-ref columns 0))])
+    (for ([center-column (range (- width 1))]
+          [center-row (range (- height 1))])
+      (let ([north-row (+ center-row 1)]
+            [east-column (+ center-column 1)]
+            [south-row (- center-row 1)]
+            [west-column (- center-column 1)])
+        (valid! (list-ref (list-ref columns center-column) center-row)
+                (list-ref (list-ref columns center-column) north-row)
+                (list-ref (list-ref columns east-column) center-row))))))
+;;                (list-ref (list-ref columns center-column) south-row)
+;;                (list-ref (list-ref columns west-column) center-row))))))
 
 (define (make-map width height)
   (let ([the-map (initialize-map width height)])
-    (valid-map the-map)
-    (solve asserts)
-    the-map))
+    (map-valid! the-map)
+    (evaluate (solve asserts) the-map)))
 
 (define (initialize-map width height)
   (if (equal? 0 width) null
