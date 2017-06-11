cc33ef3a49a4b0d98cbd995b7bfb2d8850f6a332
Drew Bronson <bronsonboy96@gmail.com>
2017-06-11 13:14:03 -0500

    Refactor tests to use constants for test structures instead of repeating literal values.

--- test/utilities-test.rkt
+++ test/utilities-test.rkt
@@ -23,10 +23,14 @@
   (for ([each-tile (add-rotations (list rotate-test-tile))])
     (image-show (tile-image each-tile))))
 
-(check-equal? (map-ref '((1 2) (3 4)) 0 0) 1)
-(check-equal? (map-ref '((1 2) (3 4)) 0 1) 2)
-(check-equal? (map-ref '((1 2) (3 4)) 1 0) 3)
-(check-equal? (map-ref '((1 2) (3 4)) 1 1) 4)
+(define test-map-1 '((1 2) (3 4)))
 
-(check-equal? (map-width '((1 2) (3 4) (5 6))) 3)
-(check-equal? (map-height '((1 2) (3 4) (5 6))) 2)
+(check-equal? (map-ref test-map-1 0 0) 1)
+(check-equal? (map-ref test-map-1 0 1) 2)
+(check-equal? (map-ref test-map-1 1 0) 3)
+(check-equal? (map-ref test-map-1 1 1) 4)
+
+(define test-map-2 '((1 2) (3 4) (5 6)))
+
+(check-equal? (map-width test-map-2) 3)
+(check-equal? (map-height test-map-2) 2)
