43ad684c13d38e5468c409166b347060d4d86314
Drew Bronson <bronsonboy96@gmail.com>
2017-06-11 21:37:09 -0500

    Implement basic colorization learning.

---
+++ source/interface.rkt
@@ -0,0 +1,34 @@
+#lang rosette
+
+(require gigls/unsafe
+         "filters.rkt"
+         "tile.rkt"
+         "../data/tile-database.rkt")
+
+(define colorization-pairings '())
+
+(define (learn-colorization word image)
+  (let ([hue-sum 0]
+        [saturation-sum 0]
+        [lightness-sum 0]
+        [weight-sum 0])
+    (for ([i 8])
+      (let ([hue (random 360)]
+            [saturation (random 100)]
+            [lightness (random 100)])
+        (image-show (colorize image hue saturation lightness))
+        (let ([weight (read)])
+          (set! hue-sum (+ hue-sum (* weight hue)))
+          (set! saturation-sum (+ saturation-sum (* weight saturation)))
+          (set! lightness-sum (+ lightness-sum (* weight lightness)))
+          (set! weight-sum (+ weight-sum weight)))))
+    (let* ([hue (/ hue-sum weight-sum)]
+           [saturation (/ saturation-sum weight-sum)]
+           [lightness (/ lightness-sum weight-sum)]
+           [learned-colorization (list hue saturation lightness)]
+           [colorization-pairing (list word learned-colorization)]
+           [colorized-image (colorize image hue saturation lightness)])
+      (set! colorization-pairings (cons colorization-pairing
+                                        colorization-pairings))
+      (image-show colorized-image)
+      colorized-image)))
