42d9dc267e983a31e43f0d036c23148f836a4037
Donald Bronson <donald.bronson@here.com>
2017-06-24 13:15:22 -0500

    Refactor colorize.

--- source/filters.rkt
+++ source/filters.rkt
@@ -5,13 +5,32 @@
 (provide colorize)
 
 (define (colorize image colorization)
-  (let* ([width (car (gimp-image-width image))]
-         [height (car (gimp-image-height image))]
-         [new-image (image-new height width)])
-    (image-select-rectangle! image REPLACE 0 0 width height)
-    (gimp-edit-copy-visible image)
-    (image-select-nothing! image)
-    (let ([pasted-image (car (gimp-edit-paste (image-get-layer new-image) 1))])
-      (apply gimp-colorize pasted-image colorization)
-      (gimp-image-flatten new-image)
-      new-image)))
+  (let* ([new-image (blank-copy image)]
+         [pasted-image (pasted-copy image new-image)])
+    (apply gimp-colorize pasted-image colorization)
+    (gimp-image-flatten new-image)
+    new-image))
+
+(define (pasted-copy image blank-image)
+  (copy image)
+  (car (gimp-edit-paste (image-get-layer blank-image) 1)))
+
+(define (blank-copy image)
+  (image-new (image-height image)
+             (image-width image)))
+
+(define (copy image)
+  (select-all image)
+  (gimp-edit-copy-visible image)
+  (image-select-nothing! image))
+
+(define (select-all image)
+  (image-select-rectangle! image REPLACE 0 0
+                           (image-width image)
+                           (image-height image)))
+
+(define (image-height image)
+  (car (gimp-image-height image)))
+
+(define (image-width image)
+  (car (gimp-image-width image)))
